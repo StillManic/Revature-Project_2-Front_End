982bbf5d4ffb9e3eda7d4585ac56c57aa5237fb2
Paul J. Athmer <Paul,Athmer@gmail.com>
2021-07-13 12:37:01 -0700

    Login page added

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Employee } from '../interface/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  url: string = 'http://localhost:8080/employees/log_in';
+  
+  constructor(private http: HttpClient) { }
+
+  // POST
+  getLogin(login: Employee): Observable<Employee> {
+	
+    return this.http.post<Employee>(this.url, login);
+  }
+
+
+/**
+* Handle Http operation that failed.
+* Let the app continue.
+* @param operation - name of the operation that failed
+* @param result - optional value to return as the observable result
+*/
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
